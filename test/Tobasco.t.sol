d82d4f4c0fb15ccd6ce2f0c9bc92b9c7e0c7b5db
JasonVranek <jasonvranek@gmail.com>
2025-04-25 13:49:24 -0700

    boilerplate

---
+++ test/Tobasco.t.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {Tobasco} from "../src/Tobasco.sol";
+
+contract TobascoTest is Test {
+    function setUp() public {
+        //todo
+    }
+}
