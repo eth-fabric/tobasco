e0cac76dfa28ff1c1c6ad098dc42de96f233c379
JasonVranek <jasonvranek@gmail.com>
2025-05-19 16:52:48 -0700

    add logic for Gateway to vindicate themselves via MIP

--- src/Burn.sol
+++ src/Burn.sol
@@ -4,15 +4,27 @@ pragma solidity >=0.8.0 <0.9.0;
 import {IBurn} from "./IBurn.sol";
 import {ITobasco} from "./ITobasco.sol";
 import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import {RLPReader} from "./lib/rlp/RLPReader.sol";
+import {MerkleTrie} from "./lib/trie/MerkleTrie.sol";
+import {RLPWriter} from "./lib/rlp/RLPWriter.sol";
+import {TransactionDecoder} from "./lib/TransactionDecoder.sol";
 
 contract Burn is IBurn {
+    using RLPReader for bytes;
+    using RLPReader for RLPReader.RLPItem;
+
+    // Challenges
     mapping(bytes32 challengeID => Challenge challenge) private challenges;
+    // Gateway faults
     mapping(bytes32 challengeID => GatewayFault fault) private faults;
+
+    // Private variables
     address private _urc;
     address private _tobasco;
     uint256 private _slashAmountWei;
     uint64 private _commitmentType;
     uint256 private _challengeWindowSeconds;
+    uint256 private constant BLOCKHASH_EVM_LOOKBACK = 256;
 
     constructor(
         address urc_,
@@ -38,7 +50,7 @@ contract Burn is IBurn {
         address _committer,
         bytes calldata _evidence,
         address _challenger
-    ) external returns (uint256 slashAmountWei) {
+    ) external returns (uint256) {
         // Only the URC can call this function
         if (msg.sender != _urc) revert OnlyURC();
 
@@ -50,32 +62,31 @@ contract Burn is IBurn {
         }
 
         // Verify the challenge period expired
-        if (block.timestamp - challenges[_challengeID].timestamp < _challengeWindowSeconds)
+        if (block.timestamp - challenges[_challengeID].timestamp < _challengeWindowSeconds) {
             revert ChallengePeriodNotExpired();
-        
+        }
+
         // Update the challenge status to ProposerFault to prevent replays
         challenges[_challengeID].status = Status.ProposerFault;
 
         // Return the slash amount to the URC slasher
-        slashAmountWei = _slashAmountWei;
+        return _slashAmountWei;
     }
 
     // @dev This function is called to challenge a Proposer failed to submit a transaction at the specified timestamp at ToB.
     // @dev This function does not check _delegation or _commitment signatures as this is handled in the `attributeGatewayFault` function or `slash()` function if the challenge is valid.
     // @dev If someone tries to grief by calling this function with an invalid _delegation or _commitment, the challenges can be ignored as they are unenforceable by `slash()` or `attributeGatewayFault()`
-    function openChallenge(
-        Delegation calldata _delegation,
-        Commitment calldata _commitment
-    ) external returns (bytes32 _challengeID) {
+    function openChallenge(Delegation calldata _delegation, Commitment calldata _commitment)
+        external
+        returns (bytes32 _challengeID)
+    {
         // Decode the commitment payload
-        ToBCommitment memory toBCommitment = abi.decode(
-            _commitment.payload,
-            (ToBCommitment)
-        );
+        ToBCommitment memory toBCommitment = abi.decode(_commitment.payload, (ToBCommitment));
 
         // Slashing is only possible if the transaction was not submitted at the specified timestamp
-        if (_wasSubmitted(toBCommitment.timestamp))
+        if (_wasSubmitted(toBCommitment.timestamp)) {
             revert CommitmentWasNotBroken();
+        }
 
         // Verify the ToB destination address was the expected Tobasco contract
         if (toBCommitment.tobasco != _tobasco) revert InvalidDestination();
@@ -84,8 +95,9 @@ contract Burn is IBurn {
         if (_commitment.slasher != address(this)) revert WrongSlasher();
 
         // Verify the commitment type matches this contract's
-        if (_commitment.commitmentType != _commitmentType)
+        if (_commitment.commitmentType != _commitmentType) {
             revert InvalidCommitmentType();
+        }
 
         // Compute the challenge ID
         _challengeID = _computeChallengeID(_delegation, _commitment);
@@ -96,10 +108,7 @@ contract Burn is IBurn {
         }
 
         // The challenge is valid -> log the challenge to start the challenge window
-        challenges[_challengeID] = Challenge({
-            timestamp: uint48(block.timestamp),
-            status: Status.Unresolved
-        });
+        challenges[_challengeID] = Challenge({timestamp: uint48(block.timestamp), status: Status.Unresolved});
 
         emit ChallengeOpened(_challengeID);
     }
@@ -107,9 +116,9 @@ contract Burn is IBurn {
     // @dev Called to vindicate a Proposer and attribute the fault to a Gateway.
     // @dev It is assumed that the Gateway signs the blockhash with their `committer` private key
     // @dev before an L1 Proposer proposes the L1 block. If the Proposer can supply the signed blockhash
-    // @dev then the Gateway attested to an incorrect block and is responsible for the fault. 
+    // @dev then the Gateway attested to an incorrect block and is responsible for the fault.
     // @dev This function tentatively attributes the fault to the Gateway since there is still the possibility
-    // @dev that the transaction executed at ToB but simply reverted due to user error. 
+    // @dev that the transaction executed at ToB but simply reverted due to user error.
     // @dev The function saves the faultId using only the `SignedCommitment` to prevent someone replaying the fault
     // @dev with falsified `Delegation` messages.
     function attributeGatewayFault(
@@ -123,22 +132,19 @@ contract Burn is IBurn {
         bytes32 _challengeID = _computeChallengeID(_delegation, _commitment.commitment);
 
         // The challenge must exist
-        if (challenges[_challengeID].status != Status.Unresolved)
+        if (challenges[_challengeID].status != Status.Unresolved) {
             revert WrongChallengeStatus();
+        }
 
         // The challenge must have been created within the challenge window
-        if (block.timestamp - challenges[_challengeID].timestamp > _challengeWindowSeconds)
+        if (block.timestamp - challenges[_challengeID].timestamp > _challengeWindowSeconds) {
             revert ChallengePeriodExpired();
+        }
 
         // Verify the Gateway signed the blockhash and the commitment
         // This provides evidence that the Gateway attested to the block's correctness
         // and therefore is the party at fault
-        if (!_verifyGatewaySignatures(
-                _gateway,
-                _commitment,
-                _blockhashSignature
-            )
-        ) revert InvalidSignature();
+        if (!_verifyGatewaySignatures(_gateway, _commitment, _blockhashSignature)) revert InvalidSignature();
 
         // Close the challenge from the Proposer's perspective
         challenges[_challengeID].status = Status.GatewayFault;
@@ -158,22 +164,46 @@ contract Burn is IBurn {
 
     // @dev Callable if `attributeGatewayFault()` has been called and the challenge period has expired.
     // @dev This implies they could not supply evidence that the transaction executed at ToB but simply reverted.
-    // @dev This contract does not assume the Gateway is registered in the URC, Rather it simply sets the 
+    // @dev This contract does not assume the Gateway is registered in the URC, Rather it simply sets the
     // @dev GatewayFault.status to `GatewayFault` that can be queried by an external contract where the Gateway
     // @dev is collateralized.
     function markGatewaySlashable(bytes32 _faultID) external {
-        if (faults[_faultID].status != Status.Unresolved)
+        if (faults[_faultID].status != Status.Unresolved) {
             revert WrongChallengeStatus();
+        }
 
         // The challenge window must have expired to mark them slashable
-        if (block.timestamp - faults[_faultID].timestamp < _challengeWindowSeconds)
-            revert ChallengePeriodNotExpired();
+        if (block.timestamp - faults[_faultID].timestamp < _challengeWindowSeconds) revert ChallengePeriodNotExpired();
 
         faults[_faultID].status = Status.GatewayFault;
     }
 
-    function vindicateGateway() external {
-        // todo
+    // @dev Called by the Gateway to vindicate themselves and set the GatewayFault.status to `Vindicated`.
+    // @dev This scenario is possible if the Gateway did execute the transaction at ToB but it reverted due to user error.
+    // @dev This function is callable after a successful call to `attributeGatewayFault()` if the challenge period has expired.
+    // @dev This function will verify that a transaction to the expected ToB contract executed at the ToB.
+    function vindicateGateway(SignedCommitment calldata _commitment, InclusionProof calldata _inclusionProof)
+        external
+    {
+        bytes32 _faultID = _computeFaultID(_commitment);
+        GatewayFault memory _fault = faults[_faultID];
+
+        // The GatewayFault must exist and be unresolved
+        if (_fault.status != Status.Unresolved) revert WrongChallengeStatus();
+
+        // The challenge window must still be active
+        if (block.timestamp - faults[_faultID].timestamp > _challengeWindowSeconds) revert ChallengePeriodExpired();
+
+        // Decode the commitment payload
+        ToBCommitment memory _toBCommitment = abi.decode(_commitment.commitment.payload, (ToBCommitment));
+
+        // Verify the inclusion proof
+        if (!_verifyInclusionProof(_inclusionProof, _toBCommitment)) {
+            revert InclusionProofInvalid();
+        }
+
+        // Vindicate the Gateway
+        faults[_faultID].status = Status.Vindicated;
     }
 
     // External view functions
@@ -189,7 +219,6 @@ contract Burn is IBurn {
         return challenges[_challengeID];
     }
 
-    // todo getters for the private variables
     function urc() external view returns (address) {
         return _urc;
     }
@@ -201,7 +230,7 @@ contract Burn is IBurn {
     function slashAmountWei() external view returns (uint256) {
         return _slashAmountWei;
     }
-    
+
     function commitmentType() external view returns (uint64) {
         return _commitmentType;
     }
@@ -222,14 +251,9 @@ contract Burn is IBurn {
             revert InvalidSignature();
         }
         // Decode the commitment payload
-        ToBCommitment memory _toBCommitment = abi.decode(
-            _commitment.commitment.payload,
-            (ToBCommitment)
-        );
+        ToBCommitment memory _toBCommitment = abi.decode(_commitment.commitment.payload, (ToBCommitment));
 
-        bytes32 _blockhash = ITobasco(_tobasco).submittedBlockhash(
-            _toBCommitment.timestamp
-        );
+        bytes32 _blockhash = ITobasco(_tobasco).submittedBlockhash(_toBCommitment.timestamp);
 
         // Verify the Gateway signed the blockhash
         return ECDSA.recover(_blockhash, _signature) == _gateway;
@@ -239,16 +263,106 @@ contract Burn is IBurn {
         return ITobasco(_tobasco).submitted(_timestamp);
     }
 
-    function _computeChallengeID(
-        Delegation calldata delegation,
-        Commitment calldata commitment
-    ) internal pure returns (bytes32) {
+    function _computeChallengeID(Delegation calldata delegation, Commitment calldata commitment)
+        internal
+        pure
+        returns (bytes32)
+    {
         return keccak256(abi.encode(commitment, delegation));
     }
 
-    function _computeFaultID(
-        SignedCommitment calldata commitment
-    ) internal pure returns (bytes32) {
+    function _computeFaultID(SignedCommitment calldata commitment) internal pure returns (bytes32) {
         return keccak256(abi.encode(commitment));
+    }
+
+    // @dev This function verifies a Merkle inclusion proof, proving:
+    // @dev 1. A transaction was submitted at the ToB position during in the expected block.
+    // @dev 2. The transaction called the expected contract.
+    // @dev 3. The transaction called the expected function with the expected function selector.
+    function _verifyInclusionProof(InclusionProof calldata proof, ToBCommitment memory toBCommitment)
+        internal
+        view
+        returns (bool)
+    {
+        // Check that the previous block is within the EVM lookback window for block hashes.
+        // Clearly, if the previous block is available, the target block will be too.
+        uint256 previousBlockNumber = proof.inclusionBlockNumber - 1;
+        if (previousBlockNumber > block.number || previousBlockNumber < block.number - BLOCKHASH_EVM_LOOKBACK) {
+            revert InvalidBlockNumber();
+        }
+
+        // Get the trusted block hash for the block number in which the transactions were included.
+        bytes32 trustedPreviousBlockHash = blockhash(proof.inclusionBlockNumber - 1);
+
+        // Check the integrity of the trusted block hash
+        bytes32 previousBlockHash = keccak256(proof.previousBlockHeaderRLP);
+        if (previousBlockHash != trustedPreviousBlockHash) {
+            revert InvalidBlockHash();
+        }
+
+        // Decode the RLP-encoded block header of the target block.
+        //
+        // The target block is necessary to extract the transaction root and verify the inclusion of the
+        // committed transaction. By checking against the previous block's parent hash we can ensure this
+        // is the correct block trusting a single block hash.
+        BlockHeaderData memory targetBlockHeader = _decodeBlockHeaderRLP(proof.inclusionBlockHeaderRLP);
+
+        // Check that the target block is a child of the previous block
+        if (targetBlockHeader.parentHash != previousBlockHash) {
+            revert InvalidParentBlockHash();
+        }
+
+        // Check that the commitment timestamp matches the block timestamp
+        if (toBCommitment.timestamp != targetBlockHeader.timestamp) {
+            revert IncorrectTimestamp();
+        }
+
+        // We expect the transaction to be in the 0th position of the block
+        bytes memory txLeaf = RLPWriter.writeUint(0);
+
+        // Verify transaction inclusion proof
+        //
+        // The transactions trie is built with raw leaves, without hashing them first
+        // (This denotes why we use `MerkleTrie.get()` as opposed to `SecureMerkleTrie.get()`).
+        (bool txExists, bytes memory txRLP) = MerkleTrie.get(txLeaf, proof.txMerkleProof, targetBlockHeader.txRoot);
+
+        // Transaction does not exist in the ToB position
+        if (!txExists) {
+            revert TransactionNotIncluded();
+        }
+
+        // RLP decode the transaction to verify it called the correct contract
+        TransactionDecoder.Transaction memory txData = TransactionDecoder.decodeEnveloped(txRLP);
+        if (txData.to != toBCommitment.tobasco) {
+            revert InvalidDestination();
+        }
+
+        // Extract the function selector from the transaction data
+        // We skip the first 32B which encode the length
+        bytes4 usedFuncSelector;
+        bytes memory data = txData.data;
+        assembly {
+            usedFuncSelector := mload(add(data, 32))
+        }
+
+        // Verify the function selector matches what was committed to
+        if (usedFuncSelector != toBCommitment.funcSelector) {
+            revert IncorrectFunctionSelector();
+        }
+
+        return true;
+    }
+
+    /// @notice Decode the block header fields from an RLP-encoded block header.
+    /// @param headerRLP The RLP-encoded block header to decode
+    function _decodeBlockHeaderRLP(bytes memory headerRLP) internal pure returns (BlockHeaderData memory blockHeader) {
+        RLPReader.RLPItem[] memory headerFields = headerRLP.toRLPItem().readList();
+
+        blockHeader.parentHash = headerFields[0].readBytes32();
+        blockHeader.stateRoot = headerFields[3].readBytes32();
+        blockHeader.txRoot = headerFields[4].readBytes32();
+        blockHeader.blockNumber = headerFields[8].readUint256();
+        blockHeader.timestamp = headerFields[11].readUint256();
+        blockHeader.baseFee = headerFields[15].readUint256();
     }
 }
--- src/IBurn.sol
+++ src/IBurn.sol
@@ -9,13 +9,16 @@ interface IBurn is ISlasher {
         uint48 timestamp;
         // The address of the ToB contract
         address tobasco;
+        // The target function selector committed to being called
+        bytes4 funcSelector;
     }
 
     enum Status {
         Nonexistent,
         Unresolved,
         ProposerFault,
-        GatewayFault
+        GatewayFault,
+        Vindicated
     }
 
     struct Challenge {
@@ -36,6 +39,28 @@ interface IBurn is ISlasher {
         Status status;
     }
 
+    struct InclusionProof {
+        // block number where the transactions are included
+        uint256 inclusionBlockNumber;
+        // RLP-encoded block header of the previous block of the inclusion block
+        // (for clarity: `previousBlockHeader.number == inclusionBlockNumber - 1`)
+        bytes previousBlockHeaderRLP;
+        // RLP-encoded block header where the committed transaction is included
+        bytes inclusionBlockHeaderRLP;
+        // merkle inclusion proof of the transaction in the transaction trie of the inclusion block
+        // (checked against the inclusionBlockHeader.txRoot)
+        bytes txMerkleProof;
+    }
+
+    struct BlockHeaderData {
+        bytes32 parentHash;
+        bytes32 stateRoot;
+        bytes32 txRoot;
+        uint256 blockNumber;
+        uint256 timestamp;
+        uint256 baseFee;
+    }
+
     // Errors
     error CommitmentWasNotBroken();
     error OnlyURC();
@@ -47,7 +72,80 @@ interface IBurn is ISlasher {
     error ChallengePeriodExpired();
     error ChallengePeriodNotExpired();
     error InvalidSignature();
+    error InvalidBlockNumber();
+    error InvalidBlockHash();
+    error InvalidParentBlockHash();
+    error TransactionNotIncluded();
+    error IncorrectTimestamp();
+    error IncorrectFunctionSelector();
+    error InclusionProofInvalid();
+
     // Events
     event ChallengeOpened(bytes32 challengeID);
     event GatewayFaultProven(bytes32 faultID, bytes32 challengeID, address gateway);
+
+    // Functions
+
+    /// @notice Opens a challenge against a Proposer who failed to execute a transaction at ToB
+    /// @param _delegation The delegation containing the Gateway's `committer` address
+    /// @param _commitment The commitment containing the ToBCommitment payload
+    /// @return The ID of the created challenge
+    function openChallenge(Delegation calldata _delegation, Commitment calldata _commitment)
+        external
+        returns (bytes32);
+
+    /// @notice Attributes a fault to a Gateway and vindicates the Proposer
+    /// @param _delegation The delegation containing the Gateway's `committer` address
+    /// @param _commitment The Gateway-signed commitment containing the ToBCommitment payload
+    /// @param _blockhashSignature The Gateway-signed blockhash for the block the challenge is for
+    function attributeGatewayFault(
+        Delegation calldata _delegation,
+        SignedCommitment calldata _commitment,
+        bytes calldata _blockhashSignature
+    ) external;
+
+    /// @notice Marks a Gateway as slashable after the challenge period has expired
+    /// @param _faultID The ID of the fault to mark as slashable
+    function markGatewaySlashable(bytes32 _faultID) external;
+
+    /// @notice Allows a Gateway to vindicate themselves by proving transaction execution
+    /// @param _commitment The signed commitment containing the payload and slasher
+    /// @param _inclusionProof The proof of transaction inclusion in the block
+    function vindicateGateway(SignedCommitment calldata _commitment, InclusionProof calldata _inclusionProof)
+        external;
+
+    /// @notice Checks if a Gateway is slashable for a given fault
+    /// @param _faultID The ID of the fault to check
+    /// @return True if the Gateway is slashable, false otherwise
+    function gatewaySlashable(bytes32 _faultID) external view returns (bool);
+
+    /// @notice Gets the fault details for a given fault ID
+    /// @param _faultID The ID of the fault to get
+    /// @return The GatewayFault struct containing fault details
+    function getFault(bytes32 _faultID) external view returns (GatewayFault memory);
+
+    /// @notice Gets the challenge details for a given challenge ID
+    /// @param _challengeID The ID of the challenge to get
+    /// @return The Challenge struct containing challenge details
+    function getChallenge(bytes32 _challengeID) external view returns (Challenge memory);
+
+    /// @notice Gets the URC address
+    /// @return The address of the URC contract
+    function urc() external view returns (address);
+
+    /// @notice Gets the Tobasco contract address
+    /// @return The address of the Tobasco contract
+    function tobasco() external view returns (address);
+
+    /// @notice Gets the slash amount in wei
+    /// @return The amount to slash in wei
+    function slashAmountWei() external view returns (uint256);
+
+    /// @notice Gets the commitment type
+    /// @return The commitment type identifier
+    function commitmentType() external view returns (uint64);
+
+    /// @notice Gets the challenge window duration in seconds
+    /// @return The duration of the challenge window in seconds
+    function challengeWindowSeconds() external view returns (uint256);
 }
